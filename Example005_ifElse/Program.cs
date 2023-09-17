14329dda18187d462336fcc95f944929db963679
Ivan <gismo8992@mail.ru>
2023-09-17 21:50:11 +0300

    Example005 project added

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input user name");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "masha") {
+    Console.WriteLine("OMG, this is Masha!");
+}
+else {
+    Console.Write("Hello, ");
+    Console.WriteLine(userName);
+}
