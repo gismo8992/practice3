f43bda05f1534ab4c203bfc7fa3ba8c565573123
Ivan <gismo8992@mail.ru>
2023-09-21 00:20:02 +0300

    Example009 project added

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -1,17 +1,31 @@
-﻿int[] array1 = {12,23,13,42,15,46,67,18,29};
-array[0] = 31;
-int Max (int[] arr) {
-    int max = 0;
-    for(int i: arr) {
-        if (max < i) {
-            max = i;
-        }
-        return max;
-    }
+﻿int Max(int arg1, int arg2, int arg3) {
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
 }
+int[] array1 = {12,23,13,42,15,46,67,18,29};
+array1[0] = 30;
+Console.WriteLine(array1[0]);
 
-int max1 = Max(array1);
-Console.WriteLine(max1);
+int max = Max(
+    Max(array1[0], array1[1], array1[2]),
+    Max(array1[3], array1[4], array1[5]),
+    Max(array1[6], array1[7], array1[8])
+);
+Console.WriteLine(max);
+
+int[] array2 = {12,23,13,42,15,46,67,18,29,42};
+int index1 = 0;
+int find = 42;
+int n = array2.Length;
+while(index1 < n) {
+    if(array2[index1] == find) {
+        Console.WriteLine(array2[index1]);
+        break;
+    }
+     index1++;
+}
 
 // int Max( int arg1, int arg2, int arg3) {
 //     int result = arg1;
