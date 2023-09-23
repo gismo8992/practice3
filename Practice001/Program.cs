4608b5ffbd60c63fb9f75eab8e9326b9792456e1
Ivan <gismo8992@mail.ru>
2023-09-23 23:21:19 +0300

    Practice001 added

---
+++ Practice001/Program.cs
@@ -0,0 +1,40 @@
+﻿// int i = Convert.ToInt32(Console.ReadLine);
+// Console.WriteLine(i+5);
+// comment
+/*
+comment
+*/
+// ctrl+/
+// Task #1
+// int i = Convert.ToInt32(Console.ReadLine); 
+// Console.WriteLine(i*i);
+
+//Task #2
+// Console.WriteLine("Input first number");
+// Console.WriteLine("Input second number");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// if(firstNumber*firstNumber == secondNumber) Console.WriteLine("Корень квадратный из второго числа равно первому числу");
+// else Console.WriteLine("Не является.");
+
+// //Task #3
+// Console.WriteLine("Input day name");
+// int dayNumber = Convert.ToInt32(Console.ReadLine());
+// if(dayNumber = 1) Console.WriteLine("Понедельник");
+// else if(dayNumber = 2) Console.WriteLine("Вторник");
+// else if(dayNumber = 3) Console.WriteLine("Среда");
+// else if(dayNumber = 4) Console.WriteLine("Четверг");
+// else if(dayNumber = 5) Console.WriteLine("Пятница");
+// else if(dayNumber = 6) Console.WriteLine("Суббота");
+// else if(dayNumber = 7) Console.WriteLine("Воскресенье");
+// else Console.WriteLine("Некорректный ввод данны, такого дня нет");
+
+//Task7
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Последняя цифра числа: " + number%10);
+
+
+
+
+
