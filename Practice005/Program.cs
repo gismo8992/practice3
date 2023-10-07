ee745d6dcf674c73a7c894b4a4dd442562fbceea
Ivan <gismo8992@mail.ru>
2023-10-07 15:45:15 +0300

    Practice005 added

---
+++ Practice005/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+Найдите сумму отрицательных и положительных элементов массива.
+Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+*/
+
+void InputArray(int[] array) {
+    for(int i = 0; i < array.Length; i++) {
+        array[i] = new Random().Next(-9,10);
+    }
+}
+
+void ReleaseArray(int[] array) {
+    int sumPos = 0;
+    int sumNeg = 0;
+    for(int i = 0; i < array.Length; i++) {
+        if(array[i] > 0) {
+            sumPos += array[i];
+        }
+        else {
+            sumNeg += array[i];
+        }
+        Console.WriteLine($"Sum positive = {sumPos}, Sum negatice = {sumNeg}");
+    }
+}
+
+Console.Clear();
+Console.Write("How many elemets: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
---
+++ Practice005/Program2.cs
@@ -0,0 +1,39 @@
+﻿// /*Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+// */
+
+// void InputArray(int[] array) {
+//     for(int i = 0; i < array.Length; i++) {
+//         array[i] = new Random().Next(-9,10);
+//     }
+// }
+
+// void ReleaseArray(int[] array) {
+//     int sumPos = 0;
+//     int sumNeg = 0;
+//     for(int i = 0; i < array.Length; i++) {
+//         if(array[i]) > 0 sumPos += array[i];
+//         else sumNeg += array[i];
+//         Console.WriteLine($"Sum positive = {sumPos}, Sum negatice = {sumNeg}");
+//     }
+// }
+
+// Console.Clear();
+// Console.Write("How many elemets: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[];
+// InputArray(array);
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+// ReleaseArray(array);
+
+/* Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+
+
+void changeArray(int[] array) {
+    for(int i = 0; i < array.Length; i++) {
+        array[i] = array[i]*(-1);
+    }
+}
