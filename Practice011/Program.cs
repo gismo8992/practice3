6bf87ea29245901e5f546ba29846c91f5dfa954c
Ivan <gismo8992@mail.ru>
2023-10-18 00:50:19 +0300

    Added Practice011 project

---
+++ Practice011/Program.cs
@@ -0,0 +1,31 @@
+﻿int[,] table = new int[3, 4];
+// String.Empty
+
+
+void PrintArray (int[,] table){
+    for (int rows = 0; rows < table.GetLength(0); rows++) {
+        for (int columns = 0; columns < table.GetLength(1); columns++) {
+            Console.Write($"{table[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int[,] arr) {
+    for(int i = 0; i < arr.GetLength(0); i++) {
+        for(int j = 0; j < arr.GetLength(1); j++) {
+            arr[i, j] = new Random().Next(0, 10); // [0; 10)
+        }
+    }
+}
+PrintArray(table);
+Console.WriteLine();
+FillArray(table);
+PrintArray(table);
+Console.WriteLine();
+
+int Factoral (int n) {
+    if (n == 1) return 1;
+    else return n*Factoral(n-1);
+}
+Console.WriteLine(Factoral(4));
