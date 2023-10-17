e6393f5c9d967f768ab90e0fff407e7101605502
Ivan <gismo8992@mail.ru>
2023-10-17 23:34:21 +0300

    Added 009 and 010 project

---
+++ Practice009/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine("Enter numbers: ");
+int[] arr = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int countPositive = 0;
+for(int i = 0; i < arr.Length; i++) {
+    if(arr[i] > 0) {
+        countPositive++;
+    }
+}
+Console.WriteLine(countPositive);
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Enter four number b1, k1, b2, k2: ");
+double[] arr1 = Console.ReadLine().Split(" ").Select(z => double.Parse(z)).ToArray();
+double x = Math.Round((arr1[2]-arr1[0])/(arr1[1]-arr1[3]), 2);
+double y = Math.Round((arr1[1]*x + arr1[0]), 2);
+Console.WriteLine($"Координата точки пересечения прямых: {x}; {y}");
+
---
+++ Practice010/Program.cs
@@ -0,0 +1,131 @@
+﻿// **Задача 53:** Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 8 4 2 4
+// 5 9 2 3
+// 1 4 7 2
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+void ReleaseMatrix(int[,] matrix)
+{
+    int temp = 0;
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матрицы: "); 
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
+PrintMatrix(matrix);
+
+// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим 
+// следующий массив:
+// 9 4 2
+// 2 2 6
+// 3 4 7
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    int minValue = matrix[0, 0], minRow = 0, minColumn = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           if (matrix[i, j] < minValue)
+           {
+                minValue = matrix[i, j];
+                minRow = i;
+                minColumn = j;
+           }
+        }
+    }
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i != minRow)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (j != minColumn)
+                    Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+Console.Clear();
+Console.Write("Введите размер матрицы: "); 
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
+
+// **Задача 55:** Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// В итоге получается вот такой массив:
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
